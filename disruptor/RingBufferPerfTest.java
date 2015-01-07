858835e3f05d48d68eed1a133c1fe0000ffe6e2c
shines77 <gz_shines@msn.com>
2015-01-08 05:20:32 +0800

    更新了 Disruptor 的测试程序 RingBufferPerfTest.java;

--- disruptor/RingBufferPerfTest.java
+++ disruptor/RingBufferPerfTest.java
@@ -26,19 +26,18 @@ import java.util.concurrent.LinkedBlockingQueue;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.ThreadPoolExecutor;
 
-import static com.lmax.disruptor.RingBuffer.createSingleProducer;
-import static com.lmax.disruptor.RingBuffer.createMultiProducer;
-
 import com.lmax.disruptor.RingBuffer;
 import com.lmax.disruptor.WorkerPool;
 import com.lmax.disruptor.EventFactory;
 import com.lmax.disruptor.EventHandler;
 import com.lmax.disruptor.WorkHandler;
+import com.lmax.disruptor.Sequence;
 import com.lmax.disruptor.Sequencer;
 import com.lmax.disruptor.SequenceBarrier;
 import com.lmax.disruptor.FatalExceptionHandler;
 import com.lmax.disruptor.BusySpinWaitStrategy;
 import com.lmax.disruptor.YieldingWaitStrategy;
+import com.lmax.disruptor.SleepingWaitStrategy;
 import com.lmax.disruptor.util.DaemonThreadFactory;
 import com.lmax.disruptor.util.PaddedLong;
 import com.lmax.disruptor.dsl.ProducerType;
@@ -79,24 +78,25 @@ import com.lmax.disruptor.dsl.Disruptor;
 
 ///
 /// See: http://blog.csdn.net/xiaohulunb/article/details/38762845
+/// See: http://ziyue1987.github.io/pages/2013/09/22/disruptor-use-manual.html
 ///
 public final class RingBufferPerfTest
 {
     private static final int BUFFER_SIZE = 1024;
     private static final int THREAD_NUMS = 2;
 
-    private static final int PUSH_CNT = THREAD_NUMS;
-    private static final int POP_CNT  = THREAD_NUMS;
+    private static final int PUSH_CNT = 2;
+    private static final int POP_CNT  = 2;
 
-    private static final int MSG_TOTAL_LENGTH = 8000000;
-    private static final int PUSH_MSG_CNT = MSG_TOTAL_LENGTH / PUSH_CNT;
-    private static final int POP_MSG_CNT  = MSG_TOTAL_LENGTH / POP_CNT;
+    private static final int MAX_MSG_COUNT = 8000000 * 1;
+    private static final int PUSH_MSG_CNT = MAX_MSG_COUNT / PUSH_CNT;
+    private static final int POP_MSG_CNT  = MAX_MSG_COUNT / POP_CNT;
 
     private static final long PAUSE_NANOS = 1000L;
 
     private static final ExecutorService executor = Executors.newCachedThreadPool(DaemonThreadFactory.INSTANCE);
-    private static Disruptor<ValueEvent> disruptor = null;
-    private static RingBuffer<ValueEvent> ringBuffer = null;
+    private static Disruptor<MessageEvent> disruptor = null;
+    private static RingBuffer<MessageEvent> ringBuffer = null;
 
     ///////////////////////////////////////////////////////////////////////////////////////////////
 
@@ -108,11 +108,11 @@ public final class RingBufferPerfTest
         }
     }
 
-    private static final ValueEvent[] events = new ValueEvent[MSG_TOTAL_LENGTH];
+    private static final MessageEvent[] events = new MessageEvent[MAX_MSG_COUNT];
     {
-        for (int i = 0; i < MSG_TOTAL_LENGTH; i++)
+        for (int i = 0; i < MAX_MSG_COUNT; i++)
         {
-            events[i] = new ValueEvent();
+            events[i] = new MessageEvent();
             events[i].setValue(i + 1);
         }
     }
@@ -125,20 +125,29 @@ public final class RingBufferPerfTest
         }
     }
 
+    private static final Sequence[] shared_counters = new Sequence[POP_CNT];
+    {
+        for (int i = 0; i < POP_CNT; i++)
+        {
+            shared_counters[i] = new Sequence(0);
+        }
+    }
+
+    // Construct a EventHandler array that have POP_CNT EventHandlers.
     private static final CountingEventHandler[] event_handlers = new CountingEventHandler[POP_CNT];
     {
         for (int i = 0; i < POP_CNT; i++)
         {
-            event_handlers[i] = new CountingEventHandler(i, counters);
+            event_handlers[i] = new CountingEventHandler(i, MAX_MSG_COUNT, counters, shared_counters);
         }
     }
 
-    // 构造拥有 POP_CNT 个WorkProcessor的WorkerPool
-    private static final EventCountingWorkHandler[] handlers = new EventCountingWorkHandler[POP_CNT];
+    // Construct a WorkerPool that have POP_CNT WorkProcessors.
+    private static final EventCountingWorkHandler[] work_handlers = new EventCountingWorkHandler[POP_CNT];
     {
         for (int i = 0; i < POP_CNT; i++)
         {
-            handlers[i] = new EventCountingWorkHandler(i, counters);
+            work_handlers[i] = new EventCountingWorkHandler(i, POP_MSG_CNT, counters, shared_counters);
         }
     }
 
@@ -153,16 +162,40 @@ public final class RingBufferPerfTest
         runRingQueuePass();
 
         long usedtime = System.currentTimeMillis() - start;
-        long opsPerSecond = (MSG_TOTAL_LENGTH * 1000L) / (System.currentTimeMillis() - start);
+        long opsPerSecond = (MAX_MSG_COUNT * 1000L) / usedtime;
 
-        System.out.format("Result: Run %d, Time = %d ms, Disruptor = %d ops/sec.%n", THREAD_NUMS, usedtime, opsPerSecond);
+        System.out.println("");
+        System.out.format("Time = %d ms, Disruptor = %d ops/sec.%n%n", usedtime, opsPerSecond);
+
+        long totals = 0;
+        for (int i = 0; i < POP_CNT; i++)
+        {
+            System.out.format("counters[%d] = %d%n", i, counters[i].get());
+            totals += counters[i].get();
+        }
+        System.out.println("");
+        System.out.format("totals = %d msgs%n", totals);
+        System.out.println("");
+
+        /*
+        totals = 0;
+        for (int i = 0; i < POP_CNT; i++)
+        {
+            System.out.format("shared_counters[%d] = %d%n", i, shared_counters[i].get());
+            totals += shared_counters[i].get();
+        }
+        System.out.println("");
+        System.out.format("totals = %d%n", totals);
+        System.out.println("");
+        //*/
+
         System.out.println("testImplementation() ... Over().");
     }
 
     private void runRingQueuePass() throws Exception
     {
-        final CountDownLatch latch = new CountDownLatch(1);
-        final CyclicBarrier barrier = new CyclicBarrier(3);
+        final CountDownLatch latch = new CountDownLatch(PUSH_CNT);
+        final CyclicBarrier barrier = new CyclicBarrier(PUSH_CNT);
         int i;
 
         resetCounters();
@@ -173,13 +206,14 @@ public final class RingBufferPerfTest
         }
         for (i = 0; i < POP_CNT; i++)
         {
-            event_handlers[i].reset(latch, MSG_TOTAL_LENGTH);
+            event_handlers[i].reset(latch, MAX_MSG_COUNT);
         }
 
-        final Future<?> productorFutures[] = new Future<?>[PUSH_CNT];
+        final Future<?> producerFutures[] = new Future<?>[PUSH_CNT];
         for (i = 0; i < PUSH_CNT; i++)
         {
-            productorFutures[i] = executor.submit(producers[i]);
+            //producerFutures[i] = executor.submit(producers[i]);
+        	executor.execute(producers[i]);
         }
 
         //workerPool.start(executor);
@@ -193,25 +227,35 @@ public final class RingBufferPerfTest
 
         for (i = 0; i < PUSH_CNT; i++)
         {
-            productorFutures[i].cancel(true);
+            //producerFutures[i].cancel(true);
         }
     }
 
     public static void main(final String[] args) throws Exception
     {
+    	System.out.println("");
         System.out.println("This is RingBufferPerfTest ...");
+        System.out.println("");
+        
+        System.out.format("MAX_MSG_COUNT = %d%n", 	MAX_MSG_COUNT);
+        System.out.format("BUFFER_SIZE   = %d%n", 	BUFFER_SIZE);
+        System.out.format("producers     = %d%n", 	PUSH_CNT);
+        System.out.format("consumers     = %d%n%n", POP_CNT);
 
         final RingBufferPerfTest test = new RingBufferPerfTest();
 
         ExecutorService executors = Executors.newCachedThreadPool();
-        disruptor = new Disruptor<ValueEvent>(ValueEvent.EVENT_FACTORY,
-            BUFFER_SIZE, executors, ProducerType.MULTI,
+        disruptor = new Disruptor<MessageEvent>(MessageEvent.EVENT_FACTORY,
+            BUFFER_SIZE, executors,
+            //ProducerType.SINGLE,
+            ProducerType.MULTI,
             //new BusySpinWaitStrategy()
             new YieldingWaitStrategy()
+            //new SleepingWaitStrategy()
             );
 
         //disruptor.handleEventsWith(event_handlers);
-        disruptor.handleEventsWithWorkerPool(handlers);
+        disruptor.handleEventsWithWorkerPool(work_handlers);
 
         ringBuffer = disruptor.start();
 
@@ -219,6 +263,14 @@ public final class RingBufferPerfTest
 
         disruptor.shutdown();
         executors.shutdown();
+
+        /*
+        for (Producer producer : producers)
+        {
+            System.out.format("producer.failed = %b%n", producer.failed);
+        }
+        System.out.println("");
+        //*/
     }
 
     private void resetCounters()
@@ -238,50 +290,13 @@ public final class RingBufferPerfTest
         private final long totalEvents;
         private final long pauseTimeNs;
 
+        public boolean failed = false;
+
         public Producer(final int id, final long totalEvents, final long pauseTimeNs)
         {
             this.id = id;
             this.totalEvents = totalEvents;
             this.pauseTimeNs = pauseTimeNs;
-        }
-
-        @Override
-        public void run()
-        {
-            try
-            {
-                //barrier.await();
-
-                ValueEvent valueEvent;
-                ValueEvent event;
-                final long base = id * totalEvents;
-                long index = base;
-
-                while (index < (base + totalEvents))
-                {
-                    event = events[(int)index];
-
-                    //System.out.println("Producer::Run() index = " + index + ",\tevent.getValue() = " + event.getValue());
-
-                    // 获得 sequence
-                    long sequence = ringBuffer.next();
-                    // 获取可用位置
-                    valueEvent = ringBuffer.get(sequence);
-                    // 填充可用位置
-                    valueEvent.setValue(event.getValue());
-                    // 通知消费者
-                    ringBuffer.publish(sequence);
-
-                    index++;
-                }
-
-                latch.countDown();
-            }
-            catch (final Exception ex)
-            {
-                ex.printStackTrace();
-                return;
-            }
         }
 
         public void reset(final CyclicBarrier barrier, final CountDownLatch latch)
@@ -291,25 +306,133 @@ public final class RingBufferPerfTest
 
             counter = 0;
         }
+
+        @Override
+        public void run()
+        {
+            try
+            {
+                barrier.await();
+
+                MessageEvent event = null;
+                final long base = id * totalEvents;
+                long index = base;
+
+                while (index < (base + totalEvents))
+                {
+                    event = events[(int)index];
+
+                    //System.out.println("Producer::Run() index = " + index + ",\tevent.getValue() = " + event.getValue());
+
+                    // Get the sequence
+                    long sequence = ringBuffer.next();
+                    // Get the event value by sequence
+                    MessageEvent valueEvent = ringBuffer.get(sequence);
+                    // Write the event value
+                    valueEvent.setValue(event.getValue());
+                    // Publish to comsumers
+                    ringBuffer.publish(sequence);
+                    
+                    index++;
+                }
+            }
+            catch (final Exception ex)
+            {
+                ex.printStackTrace();
+                failed = true;
+            }
+            finally
+            {
+                latch.countDown();
+            }
+        }
     }
 
-    public static final class EventCountingWorkHandler implements WorkHandler<ValueEvent>
+    public final class CountingEventHandler implements EventHandler<MessageEvent>
     {
-        private int id;
+        private final int id;
+        private final int messageCount;
         private final PaddedLong[] counters;
+        private final PaddedLong value = new PaddedLong();
+        private final Sequence[] shared_counters;
+        private int readCount = 0;
+        private CountDownLatch latch;
 
-        public EventCountingWorkHandler(int id, final PaddedLong[] counters)
+        public CountingEventHandler(final int id, final int messageCount,
+                                    final PaddedLong[] counters,
+                                    final Sequence[] shared_counters)
         {
             this.id = id;
+            this.messageCount = messageCount;
             this.counters = counters;
+            this.shared_counters = shared_counters;
+        }
+
+        public long getValue() {
+            return value.get();
+        }
+
+        public void reset(final CountDownLatch latch, final int expectedCount)
+        {
+            value.set(0L);
+            this.latch = latch;
+            readCount = expectedCount;
         }
 
         @Override
-        public void onEvent(final ValueEvent event) throws Exception
+        public void onEvent(final MessageEvent event, final long sequence, final boolean endOfBatch) throws Exception
+        {
+            this.counters[id].set(this.counters[id].get() + 1L);
+            //this.shared_counters[id].incrementAndGet();
+
+            /*
+            Thread.sleep(1);
+            if (event != null)
+            {
+                System.out.println("CountingEventHandler::onEvent() -- id: " + id
+                        + ", event: " + event.getValue()
+                        + ", sequence: " + sequence
+                        + ", counter: " + counters[id].get());
+            }
+            //*/
+        }
+    }
+
+    public final class EventCountingWorkHandler implements WorkHandler<MessageEvent>
+    {
+        private final int id;
+        private final int messageCount;
+        private final PaddedLong[] counters;
+        private final Sequence[] shared_counters;
+        public int readCount = 0;
+
+        public EventCountingWorkHandler(final int id, final int messageCount,
+                                        final PaddedLong[] counters,
+                                        final Sequence[] shared_counters)
+        {
+            this.id = id;
+            this.messageCount = messageCount;
+            this.counters = counters;
+            this.shared_counters = shared_counters;
+        }
+
+        @Override
+        public void onEvent(final MessageEvent event) throws Exception
         {
             //System.out.println("EventCountingWorkHandler::onEvent() ... Enter()");
 
-            counters[id].set(counters[id].get() + 1L);
+            this.counters[id].set(this.counters[id].get() + 1L);
+            //this.shared_counters[id].incrementAndGet();
+            /*
+            if (readCount < messageCount)
+            {
+                readCount++;
+            }
+            else
+            {
+                this.counters[id].set(messageCount);
+            }
+            //*/
 
             /*
             long sequence = 1;
@@ -324,54 +447,7 @@ public final class RingBufferPerfTest
         }
     }
 
-    public static final class CountingEventHandler implements EventHandler<ValueEvent>
-    {
-        private int id;
-        private final PaddedLong[] counters2;
-        private final PaddedLong value = new PaddedLong();
-        private long count;
-        private CountDownLatch latch;
-
-        public CountingEventHandler(int id, final PaddedLong[] counters)
-        {
-            this.id = id;
-            this.counters2 = counters;
-        }
-
-        public long getValue() {
-            return value.get();
-        }
-
-        public void reset(final CountDownLatch latch, final long expectedCount)
-        {
-            value.set(0L);
-            this.latch = latch;
-            count = expectedCount;
-        }
-
-        @Override
-        public void onEvent(final ValueEvent event, final long sequence, final boolean endOfBatch) throws Exception
-        {
-            counters[id].set(counters[id].get() + 1L);
-
-            /*
-            Thread.sleep(1);
-            if (event != null)
-            {
-                System.out.println("CountingEventHandler::onEvent() -- id: " + id
-                        + ", event: " + event.getValue()
-                        + ", sequence: " + sequence
-                        + ", counter: " + counters[id].get());
-            }
-            //*/
-
-            if (count == sequence) {
-                //latch.countDown();
-            }
-        }
-    }
-
-    public static final class ValueEvent
+    public static final class MessageEvent
     {
         private long value;
 
@@ -385,11 +461,11 @@ public final class RingBufferPerfTest
             this.value = value;
         }
 
-        public static final EventFactory<ValueEvent> EVENT_FACTORY = new EventFactory<ValueEvent>()
+        public static final EventFactory<MessageEvent> EVENT_FACTORY = new EventFactory<MessageEvent>()
         {
-            public ValueEvent newInstance()
+            public MessageEvent newInstance()
             {
-                return new ValueEvent();
+                return new MessageEvent();
             }
         };
     }
