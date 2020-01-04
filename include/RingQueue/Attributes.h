9a418bb46c7f10856b7b39f10319ebc94c5cd004
shines77 <gz_shines@msn.com>
2020-01-05 00:05:29 +0800

    JIMI_INLINE, JIMI_NOINLINE 等宏, 移至 Attributes.h 中;
    Signed-off-by: shines77 <gz_shines@msn.com>

--- include/RingQueue/Attributes.h
+++ include/RingQueue/Attributes.h
@@ -129,4 +129,61 @@
 #  define JIMI_NORETURN_PTR     /* no support */
 #endif
 
+//
+// inline & noinline
+// See: https://stackoverflow.com/questions/1474030/how-can-i-tell-gcc-not-to-inline-a-function
+//
+#ifdef __cplusplus
+# define JIMI_CRT_INLINE        inline
+#else
+# if __GNUC_STDC_INLINE__
+#  define JIMI_CRT_INLINE       extern inline __attribute__((__gnu_inline__))
+# else
+#  define JIMI_CRT_INLINE       extern __inline__
+# endif
+#endif
+
+/* JIMI_CRT_FORCEINLINE will be used when we have a function whose purpose is to return
+ * the value of a similar function. This alias function will contain one line
+ * of code.
+ */
+#define JIMI_CRT_FORCEINLINE    JIMI_CRT_INLINE __attribute__((__always_inline__))
+
+/**
+ * jimi: for inline and noinline define
+ */
+#if defined(_MSC_VER) || defined(__INTEL_COMPILER)
+
+#define JIMI_INLINE              __inline
+#define JIMI_FORCEINLINE         __forceinline
+#define JIMI_RESTRICT            __restrict
+#define JIMI_HAS_INLINE          1
+
+#elif defined(__GNUC__) || defined(__clang__)
+
+#define JIMI_INLINE              inline
+#define JIMI_FORCEINLINE         inline
+#define JIMI_RESTRICT            __restrict
+#define JIMI_HAS_INLINE          1
+
+#else
+
+#define JIMI_INLINE              inline
+#define JIMI_FORCEINLINE         inline
+#define JIMI_RESTRICT            /* no support */
+#define JIMI_HAS_INLINE          1
+
+#endif
+
+#if defined(_MSC_VER)
+#define JIMI_NOINLINE                   __declspec(noinline)
+#define JIMI_NOINLINE_DECLARE(decl)     __declspec(noinline) decl
+#elif (defined(__GNUC__) || defined(__clang__) || defined(__MINGW32__)) && __has_attribute(noinline)
+#define JIMI_NOINLINE                   __attribute__((noinline))
+#define JIMI_NOINLINE_DECLARE(decl)     decl __attribute__((noinline))
+#else
+#define JIMI_NOINLINE                   /* no support */
+#define JIMI_NOINLINE_DECLARE(decl)     decl
+#endif
+
 #endif // JIMI_ATTRIBUTES_H
